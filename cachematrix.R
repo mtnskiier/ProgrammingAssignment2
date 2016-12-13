137962c6a624626f72391ff8c95763c0a5b69977
Jim Baker <jim@starluna.com>
2016-12-13 15:57:06 -0700

    implemented R week 3 progrmming assignment - lexical scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Caching the inverse of a Matrix
+## 
+## Purpose - Use the lexical scoping within R to cache the matrix inverse operation.
+##
 
 makeCacheMatrix <- function(x = matrix()) {
+        ## INPUTS 
+        ##      x - Is a inverted matrix to cache for future reference
+        
+        ## OUTPUTS
+        ##      A list of methods accessing the original (x) and inverse value (mi)
+        
+        mi <- NULL
+        
+        set <- function(y) {
+                x <<- y
+                mi <<- NULL
+        }
+        
+        get <- function() x
+        setim <- function(inv_mat) mi <<- inv_mat
+        getim <- function() mi
+        
+        list(set = set, get = get,
+             setim = setim,
+             getim = getim)
 
 }
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## INPUTS
+        ## x - a vector contining a source matrix and it's inverse, and all methods 
+        ##     (see makeCacheMatrix)
+        ##
+        ## OUTPUTS
+        ## mi - is the inverse of the original matrix
+  
+        
+        mi <- x$getim()
+       
+        if (!is.null(mi)) {
+                message("getting cached data")
+                return(as.matrix(mi))
+        }
+        
+        data <- x$get()
+        print(data)
+        ## solve for the inverse
+        mi <- solve(data, ...) 
+        ## cache the results
+        x$setim(mi)
+        ## return the inverse matrix
+        return(as.matrix(mi))                                             
 }
